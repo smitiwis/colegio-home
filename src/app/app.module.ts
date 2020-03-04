f73c2c104359ffc4107c2065a3776805400e34c0
smitiwis <sistemas.luismi@gmail.com>
2020-03-04 12:03:06 -0500

    Nuestros pilares

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { IntranetComponent } from './components/intranet/intranet.component';
+import { Error404Component } from './components/error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NosotrosComponent,
+    ContactoComponent,
+    IntranetComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
