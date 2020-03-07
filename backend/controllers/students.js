a554f473fa69c37fa72cadbb6e129b13d9dd3c3f
andreuXvz <dreuan9@gmail.com>
2020-03-07 01:23:12 -0500

    studentsÂ

---
+++ backend/controllers/students.js
@@ -0,0 +1,111 @@
+const Student = require("../models/students");
+
+exports.createStudent = (req, res, next) => {
+  const student = new Student({
+    name: req.body.name,
+    ape_pat: req.body.ape_pat,
+    ape_mat: req.body.ape_mat,
+    dni: req.body.dni,
+    birthday: req.body.birthday,
+    email: req.body.email,
+    cel: req.body.cel,
+    // creator: req.userData.userId
+  });
+
+  student
+    .save()
+    .then(() => {
+      res.status(201).json({
+        message: "Se creo correctamente al estudiante",
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "La creación del docente tuvo un error!"
+      });
+    });
+};
+
+exports.updateStudent = (req, res, next) => {
+
+  const student = new Student({
+    _id: req.params.id,
+    name: req.body.name,
+    ape_pat: req.body.ape_pat,
+    ape_mat: req.body.ape_mat,
+    dni: req.body.dni,
+    birthday: req.body.birthday,
+    email: req.body.email,
+    cel: req.body.cel,
+  });
+
+  student.updateOne({ _id: req.params.id }, student)
+    .then(result => {
+      res.status(200).json({ message: "Se actualizo correctamente" });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "No sé actualizo el docente"
+      });
+    });
+};
+
+exports.getStudents = (req, res, next) => {
+  const pageSize = +req.query.pagesize;
+  const currentPage = +req.query.page;
+  const studentQuery = Students.find();
+  let fetchedStudents;
+  if (pageSize && currentPage) {
+    studentQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
+  }
+  studentQuery
+    .then(documents => {
+      fetchedStudents = documents;
+      return Students.count();
+    })
+    .then(count => {
+      res.status(200).json({
+        message: "Lista de estudiantes obtenido correctamente!",
+        students: fetchedStudents,
+        maxStudents: count
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Fallo el servicio!"
+      });
+    });
+};
+
+exports.getStudent = (req, res, next) => {
+  Student.findById(req.params.id)
+    .then(student => {
+      if (student) {
+        res.status(200).json(student);
+      } else {
+        res.status(404).json({ message: "Estudiante no encontrado" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Fallo el servicio"
+      });
+    });
+};
+
+exports.deleteStudent = (req, res, next) => {
+  Student.deleteOne({ _id: req.params.id, creator: req.userData.userId })
+    .then(result => {
+      console.log(result);
+      if (result.n > 0) {
+        res.status(200).json({ message: "Se elimino correctamente!" });
+      } else {
+        res.status(401).json({ message: "Not authorized!" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Fallo el servicio!"
+      });
+    });
+};
