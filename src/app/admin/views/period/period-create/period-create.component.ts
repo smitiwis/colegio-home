adf6d0395f4452ed80e4b78cc051743a841a67d9
smitiwis <sistemas.luismi@gmail.com>
2020-04-23 22:53:47 -0500

    arreglos del anterior commit

--- src/app/admin/views/period/period-create/period-create.component.ts
+++ src/app/admin/views/period/period-create/period-create.component.ts
@@ -17,26 +17,11 @@ export class PeriodCreateComponent implements OnInit {
   registrarPeriod() {
     this.formPeriod = this._formBuilder.group({
       nombrePeriod: ['', [Validators.required, Validators.minLength(4)]],
-      bimestre1Period: ['', [Validators.requiredTrue]],
-      bimestre2Period: ['', [Validators.requiredTrue]],
-      bimestre3Period: ['', [Validators.requiredTrue]],
-      bimestre4Period: ['', [Validators.requiredTrue]]
     })
   }
   get nombrePeriodValid() { return this.formPeriod.get('nombrePeriod').valid }
   get nombrePeriodInvalid() { return this.formPeriod.get('nombrePeriod').invalid && this.formPeriod.get('nombrePeriod').touched }
 
-  get bimestre1PeriodValid() { return this.formPeriod.get('bimestre1Period').valid }
-  get bimestre1PeriodInvalid() { return this.formPeriod.get('bimestre1Period').invalid && this.formPeriod.get('bimestre1Period').touched }
-
-  get bimestre2PeriodValid() { return this.formPeriod.get('bimestre2Period').valid }
-  get bimestre2PeriodInvalid() { return this.formPeriod.get('bimestre2Period').invalid && this.formPeriod.get('bimestre2Period').touched }
-
-  get bimestre3PeriodValid() { return this.formPeriod.get('bimestre3Period').valid }
-  get bimestre3PeriodInvalid() { return this.formPeriod.get('bimestre3Period').invalid && this.formPeriod.get('bimestre3Period').touched }
-
-  get bimestre4PeriodValid() { return this.formPeriod.get('bimestre4Period').valid }
-  get bimestre4PeriodInvalid() { return this.formPeriod.get('bimestre4Period').invalid && this.formPeriod.get('bimestre4Period').touched }
 
 
 
